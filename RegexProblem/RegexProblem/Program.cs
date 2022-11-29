6f65b4e4125f0a7c5043033d89b6123fc53536f8
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-29 21:52:48 +0530

    [Suraj]Add:Validate User Last Name

--- RegexProblem/RegexProblem/Program.cs
+++ RegexProblem/RegexProblem/Program.cs
@@ -5,21 +5,28 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcone To User Registration Problem Statement");
-            UserFirstName userFirstName = new UserFirstName();
+            UserFirstName name = new UserFirstName();
             bool flag = true;
             while (flag)
             {
                 Console.WriteLine("********************************************");
                 Console.WriteLine("Select option you want to enter.");
-                Console.WriteLine("1.First Name");
+                Console.WriteLine(" 1.First Name\n 2.Last Name");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
                         Console.WriteLine("Enter your First name");
                         string firstName = Console.ReadLine();
-                        userFirstName.ValidateFirstName(firstName);
+                        name.ValidateFirstName(firstName);
                         break;
+                    case 2:
+                        Console.WriteLine("Enter your Last name");
+                        string lastName = Console.ReadLine();
+                        name.UserLastName(lastName);
+                        break;
+
+
                 }
             }
         }
--- RegexProblem/RegexProblem/UserFirstName.cs
+++ RegexProblem/RegexProblem/UserFirstName.cs
@@ -10,12 +10,20 @@ namespace RegexProblem
     public class UserFirstName
     {
         const string firstName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        const string lastName = "^[A-Z]{1}[a-zA-Z]{2,}$";
         public void ValidateFirstName(string input)
         {
             if (Regex.IsMatch(input, firstName))
                 Console.WriteLine("Your First name is  " + input);
             else
                 Console.WriteLine("Entered name is in invalid format.Try again ");
+        }
+        public void UserLastName(string input)
+        {
+            if (Regex.IsMatch(input, lastName))
+                Console.WriteLine("Your Last name is  " + input);
+            else
+                Console.WriteLine("Entered name is in invalid format.");
         }
     }
 }
