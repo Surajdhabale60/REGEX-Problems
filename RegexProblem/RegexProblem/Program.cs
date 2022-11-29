83a573a1e8003c353dade84b6c5d7afaa2d2449e
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-29 21:40:36 +0530

    [Suarj]Add:Validate User First Name

--- RegexProblem/RegexProblem/Program.cs
+++ RegexProblem/RegexProblem/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace RegexProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcone To User Registration Problem Statement");
+            UserFirstName userFirstName = new UserFirstName();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("********************************************");
+                Console.WriteLine("Select option you want to enter.");
+                Console.WriteLine("1.First Name");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter your First name");
+                        string firstName = Console.ReadLine();
+                        userFirstName.ValidateFirstName(firstName);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ RegexProblem/RegexProblem/UserFirstName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProblem
+{
+    public class UserFirstName
+    {
+        const string firstName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        public void ValidateFirstName(string input)
+        {
+            if (Regex.IsMatch(input, firstName))
+                Console.WriteLine("Your First name is  " + input);
+            else
+                Console.WriteLine("Entered name is in invalid format.Try again ");
+        }
+    }
+}
