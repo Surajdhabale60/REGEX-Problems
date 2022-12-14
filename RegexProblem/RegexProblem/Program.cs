5d6eb1b3fbd4c4959bb8932cd5c359a4e27f17ed
Suraj Dhabale <surajdhabale60@gmail.com>
2022-12-14 20:36:32 +0530

    [Suraj]Add:Using Lambda To Validate All Methods

--- RegexProblem/RegexProblem/Program.cs
+++ RegexProblem/RegexProblem/Program.cs
@@ -11,38 +11,53 @@
             {
                 Console.WriteLine("********************************************");
                 Console.WriteLine("Select option you want to enter.");
-                Console.WriteLine(" 1.First Name\n 2.Last Name\n 3.Email ID\n 4.Mobile Number\n 5.Password\n 6.Validate Emails");
+                Console.WriteLine(" 1.First Name\n 2.Last Name\n 3.Email ID\n 4.Mobile Number\n 5.Password\n 6.Validate Emails\n 7.Using Lambda Validating All");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
-                {
+               {
                     case 1:
                         Console.WriteLine("Enter your First name");
                         string firstName = Console.ReadLine();
-                        name.ValidateFirstName(firstName);
+                        // name.ValidateFirstName(firstName);
+                        string result = name.ValidateFirstNameUsingLambda(firstName);
+                        Console.WriteLine(result);
                         break;
                     case 2:
                         Console.WriteLine("Enter your Last name");
                         string lastName = Console.ReadLine();
-                        name.UserLastName(lastName);
+                        // name.ValidateLastName(lastName);
+                        string resultt = name.ValidateLastNameUsingLambda(lastName);
+                        Console.WriteLine(resultt);
                         break;
                     case 3:
-                        Console.WriteLine("Enter valid Email");
-                        string emaiId = Console.ReadLine();
-                        name.EmailID(emaiId);
+                        Console.WriteLine("Enter your Email Id");
+                        string email = Console.ReadLine();
+                        // name.ValidateEmail(email);
+                        string vald = name.ValidateEmailUsingLambda(email);
+                        Console.WriteLine(vald);
                         break;
                     case 4:
-                        Console.WriteLine("Enter Mobile Number");
-                        string mobNumber = Convert.ToString(Console.ReadLine());
-                        name.MobNumber(mobNumber);
+                        Console.WriteLine("Enter your Mobile Number");
+                        string mobileNumber = Console.ReadLine();
+                        // name.ValidateMobileNumber(mobileNumber);
+                        string mobile = name.ValidateMobileNumberUsingLambda(mobileNumber);
+                        Console.WriteLine(mobile);
                         break;
                     case 5:
-                        Console.WriteLine("Enter Password");
-                        string password = Convert.ToString(Console.ReadLine());
-                        name.Password(password);
+                        Console.WriteLine("Enter your Password");
+                        string password = Console.ReadLine();
+                        //name.ValidatePassword(password);
+                        string pass = name.ValidatePasswordUsingLambda(password);
+                        Console.WriteLine(pass);
                         break;
                     case 6:
                         string filepath = @"C:\Users\suraj\source\repos\217 .Net\REGEX-User-Registration-Problems\RegexProblem\RegexProblem\Email.txt";
-                        name.ReadEmailsData(filepath);
+                        // name.ReadEmailsData(emailsFilePath);
+                        name.ReadEmailsDataUsingLambda(filepath);
+                        break;
+                    case 7:
+                        name.CallingAllEmailUsingLambda("Suraj");
+                        Console.WriteLine("Enter Below Data");
                         break;
                 }
             }
--- RegexProblem/RegexProblem/UserFirstName.cs
+++ RegexProblem/RegexProblem/UserFirstName.cs
@@ -16,56 +16,83 @@ namespace RegexProblem
         const string mobNumber = "^91[ ]{1}[5-9]{1}[0-9]{9}$";
         const string password = "^(?=.*[!@#$%_])(?=.*[0-9])(?=.*[A-Z])[A-Za-z0-9!@#_$%]{8,}$";
         const string emailsCheck = "^[a-zA-Z]+[.+_-]{0,1}[a-z0-9]+[@][a-zA-Z0-9]+[.][a-z]{2,3}([.][a-z]{2,3}){0,1}$";
-        public void ValidateFirstName(string input)
-        {
-            if (Regex.IsMatch(input,firstName))
-                Console.WriteLine("Your First name is  " + input);
-            else
-                Console.WriteLine("Entered name is in invalid format.");
-        }
-        public void UserLastName(string input)
-        {
-            if (Regex.IsMatch(input, lastName))
-                Console.WriteLine("Your Last name is  " + input);
-            else
-                Console.WriteLine("Entered name is in invalid format.");
-        }
-        public void EmailID(string input)
-        {
-            if (Regex.IsMatch(input, emailId))
-                Console.WriteLine("Entered Email is " + input);
-            else
-                Console.WriteLine("Entered Email is in Invalid format.");
-        }
-        public void MobNumber(string input)
-        {
-            if (Regex.IsMatch(input, mobNumber))
-                Console.WriteLine("Entered Mobile Number is " + input);
-            else
-                Console.WriteLine("Invalid mobile number try again.");
-        }
-        public void Password(string input)
-        {
-            if (Regex.IsMatch(input,password))
-                Console.WriteLine("Verify your Password :" + input);
-            else
-                Console.WriteLine("Entered password is invalid");
-        }
-        public void ReadEmailsData(string filepath)
+        //public void ValidateFirstName(string input)
+        //{
+        //    if (Regex.IsMatch(input, firstName))
+        //        Console.WriteLine("Your First name is  " + input);
+        //    else
+        //        Console.WriteLine("Entered name is in invalid format.");
+        //}
+        //public void UserLastName(string input)
+        //{
+        //    if (Regex.IsMatch(input, lastName))
+        //        Console.WriteLine("Your Last name is  " + input);
+        //    else
+        //        Console.WriteLine("Entered name is in invalid format.");
+        //}
+        //public void EmailID(string input)
+        //{
+        //    if (Regex.IsMatch(input, emailId))
+        //        Console.WriteLine("Entered Email is " + input);
+        //    else
+        //        Console.WriteLine("Entered Email is in Invalid format.");
+        //}
+        //public void MobNumber(string input)
+        //{
+        //    if (Regex.IsMatch(input, mobNumber))
+        //        Console.WriteLine("Entered Mobile Number is " + input);
+        //    else
+        //        Console.WriteLine("Invalid mobile number try again.");
+        //}
+        //public void Password(string input)
+        //{
+        //    if (Regex.IsMatch(input, password))
+        //        Console.WriteLine("Verify your Password :" + input);
+        //    else
+        //        Console.WriteLine("Entered password is invalid");
+        //}
+        //public void ReadEmailsData(string filepath)
+        //{
+        //    var email = File.ReadAllText(filepath);
+        //    emailsData = email.Split(",");
+        //    EmailData(emailsData);
+        //}
+        //public void EmailData(string[] input)
+        //{
+        //    foreach (var email in input)
+        //    {
+        //        if (Regex.IsMatch(email, emailsCheck))
+        //            Console.WriteLine("{0} as Email Id is valid", email);
+        //        else
+        //            Console.WriteLine("{0} as Email Id is invalid ", email);
+        //    }
+        //}
+        public string ValidateFirstNameUsingLambda(string input) =>
+            Regex.IsMatch(input, firstName) ? "Name is Valid" : "Entered Invalid Name";
+        public string ValidateLastNameUsingLambda(string input) =>
+            Regex.IsMatch(input, lastName) ? "Name is Valid" : "Entered Invalid Name";
+        public string ValidateEmailUsingLambda(string input) =>
+           Regex.IsMatch(input, emailId) ? "Email is Valid" : "Entered Invalid Email";
+        public string ValidateMobileNumberUsingLambda(string input) =>
+          Regex.IsMatch(input, mobNumber) ? "Mobile Number is Valid" : "Entered Invalid Mobile Number";
+        public string ValidatePasswordUsingLambda(string input) =>
+          Regex.IsMatch(input, password) ? "Password is Valid" : "Entered Invalid Password";
+        public void ReadEmailsDataUsingLambda(string filepath)
         {
             var email = File.ReadAllText(filepath);
             emailsData = email.Split(",");
-            EmailData(emailsData);
-        }
-        public void EmailData(string[] input)
-        {
-            foreach (var email in input)
+            foreach (var data in emailsData)
             {
-                if (Regex.IsMatch(email, emailsCheck))
-                    Console.WriteLine("{0} as Email Id is valid", email);
+                if (CallingAllEmailUsingLambda(data))
+                {
+                    Console.WriteLine("{0} as Email Id is valid", data);
+                }
                 else
-                    Console.WriteLine("{0} as Email Id is invalid ", email);
+                {
+                    Console.WriteLine("{0} as Email Id is invalid ", data);
+                }
             }
         }
+        public bool CallingAllEmailUsingLambda(string email) =>Regex.IsMatch(email, emailsCheck) ? true : false;
     }
 }
